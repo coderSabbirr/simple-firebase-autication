895067eafc8437cf9e0eb2c664765072a2af272b
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-10-06 19:40:13 +0600

    all add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {getAuth,signInWithPopup, GoogleAuthProvider, GithubAuthProvider ,signOut} from "firebase/auth";
+
+import initializeAuthentication from './Firebase/Firebase-Initialize';
+import { useState } from 'react';
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
 function App() {
+  const [user,setUser] =useState({})
+  const auth = getAuth();
+
+  const handalGoogleSignIn =() => {
+ 
+    signInWithPopup(auth,googleProvider)
+    .then(result => {
+      const {displayName,email,photoURL}=result.user;
+      const loingInUser = {
+        name:displayName,
+        email:email,
+        photo:photoURL
+      };
+      setUser(loingInUser);
+    })
+     .catch( error =>{
+       console.log(error.message);
+     })
+  }
+const handalgitHubSignIn =() => {
+  signInWithPopup(auth, gitHubProvider)
+  .then(result=> {
+    const {displayName,photoURL,email}= result.user;
+
+    const loggedInuser = {
+      name:displayName,
+      email:email,
+      photo:photoURL
+    }
+   setUser(loggedInuser)
+  })
+  .catch( error =>{
+    console.log(error.message);
+  })
+ 
+
+}
+const handalSignout = () => {
+  signOut(auth)
+  .then(()=>{
+    setUser({})
+  })
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    { !user.name?
+    <div>
+     <button onClick={handalGoogleSignIn}>Google Sign in</button>
+      <button onClick={handalgitHubSignIn}>GitHub Sign in</button>
+     </div> :
+      <button onClick={handalSignout}> Sign out</button>
+    }
+       <br/>
+       {
+         user.name&& <div>
+           <h1>Welcome {user.name}</h1>
+           <p>i Khnow your email {user.email}</p>
+           <img src={user.photo} alt=""/> 
+         </div>
+       }
     </div>
   );
 }
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBjQ-9niU4CIS_kx8Vn-5IXBzg4MYsCJno",
+    authDomain: "fir-loing-11224.firebaseapp.com",
+    projectId: "fir-loing-11224",
+    storageBucket: "fir-loing-11224.appspot.com",
+    messagingSenderId: "1013871787412",
+    appId: "1:1013871787412:web:798636752893d9d11fe381"
+};
+  
+  export default firebaseConfig;
